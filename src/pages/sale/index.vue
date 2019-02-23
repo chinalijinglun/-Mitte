32bdbd27e01880d9bc3b0f3ffe68f6a585c6f170
李经纶 <281965992@qq.com>
2019-02-24 00:57:22 +0800

    售后管理

--- src/pages/sale/index.vue
+++ src/pages/sale/index.vue
@@ -1,13 +1,241 @@
 <template>
-  <div>sale</div>
+  <div class="orderContainer">
+    <el-row :gutter="40">
+      <el-col :span="16">
+        <div class="left">
+          <div class="nav">
+             <p class="title">售后管理</p>
+          </div>
+         <div class="tableContainer">
+    <el-table
+      :data="list"
+      highlight-current-row
+      @current-change="handleCurrentChange"
+      style="width: 100%"
+      :cell-style="{border:'none'}"
+      :header-cell-style="{border:'none'}"
+    >
+      <el-table-column
+        label="序列号"
+        type="index"
+        width="70"
+        align="center"
+      >
+      </el-table-column>
+      <el-table-column
+        label="订单编号"
+        prop="orderNum"
+        align="center"
+      >
+      </el-table-column>
+      <el-table-column
+        label="实付总价(元)"
+        prop="price"
+        align="center"
+      >
+      </el-table-column>
+      <el-table-column
+        label="下单时间"
+        prop="buyTime"
+        align="center"
+      >
+      </el-table-column>
+      <el-table-column
+        label="申请时间"
+        prop="status"
+        align="center"
+      >
+      </el-table-column>
+    </el-table>
+  </div>
+        </div>
+      </el-col>
+      <el-col :span="8">
+        <div class="right">
+          <p class="title">商品信息</p>
+          <div class="info">
+            <div class="top">
+              <h4>快递单号：100010010210100999</h4>
+              <p>
+                <span>我是收货人</span>
+                <span>18128475483</span>
+              </p>
+              <p>北京市朝阳区天堂路122号</p>
+              <p>积分5元</p>
+              <p>微信支付</p>
+            </div>
+            <div class="middle">
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            <div class="bottom">
+              <el-scrollbar style="height: 100%">
+                <ShopsInfo />
+                <ShopsInfo />
+                <ShopsInfo />
+                <ShopsInfo />
+                <ShopsInfo />
+                <ShopsInfo />
+              </el-scrollbar>
+            </div>
+          </div>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
+  import ShopsInfo from './saleInfo';
   export default {
-    name: "index"
+    name: "index",
+    data() {
+      return {
+        list:[
+          {
+            orderNum:123345667,
+            price:100,
+            buyTime:'2018-3-4 17:45:28',
+            status:'2018-3-4 17:45:28'
+          },
+          {
+            orderNum:123345667,
+            price:100,
+            buyTime:'2018-3-4 17:45:28',
+            status:'2018-3-4 17:45:28'
+          },
+          {
+            orderNum:123345667,
+            price:100,
+            buyTime:'2018-3-4 17:45:28',
+            status:'2018-3-4 17:45:28'
+          },
+          {
+            orderNum:123345667,
+            price:100,
+            buyTime:'2018-3-4 17:45:28',
+            status:'2018-3-4 17:45:28'
+          }
+        ],
+        obj:{
+          color:'red'
+        }
+      }
+    },
+    methods: {
+      handleCurrentChange() {}
+    },
+    components: {
+      ShopsInfo
+    }
   }
 </script>
 
-<style scoped>
-
+<style scoped lang="less">
+ .tableContainer {
+    border-radius: 7px;
+    overflow: hidden;
+    height: calc(100% - 80px);
+    background-color: #fff;
+    /deep/ .el-table {
+      &::before {
+        background-color: transparent;
+      }
+    }
+  }
+  .orderContainer {
+    height: calc(100% - 50px);
+    /deep/ .el-row {
+      height: 100%;
+      .el-col {
+        height: 100%;
+      }
+    }
+    .left {
+      height: 100%;
+      .title {
+        height: 80px;
+        line-height: 80px;
+        padding-left: 27px;
+        font-size: 24px;
+      }
+      .nav {
+        display: flex;
+        p {
+          width: 30%;
+          height: 80px;
+          line-height: 80px;
+          a {
+            color: #666;
+            text-decoration: none;
+            font-weight: 300;
+          }
+          .router-link-active {
+            color: #000;
+            font-size: 24px;
+            font-weight: 500;
+          }
+        }
+      }
+    }
+    .right {
+      height: 100%;
+      .title {
+        height: 80px;
+        line-height: 80px;
+        padding-left: 27px;
+        font-size: 24px;
+      }
+      .info {
+        height: calc(100% - 80px);
+        background-color: #fff;
+        border-radius: 7px;
+        .top {
+          height: 25%;
+          padding:10px 20px;
+          font-size: 14px;
+          box-sizing: border-box;
+          display: flex;
+          flex-direction: column;
+          justify-content: space-around;
+          color: #606266;
+        }
+        .middle {
+          height: 20px;
+          background-color: #fff;
+          display: flex;
+          align-items: center;
+          div {
+            height: 100%;
+            background-color: #f2f2f2;
+          }
+          div:first-child {
+            width: 20px;
+            margin-left: -10px;
+            border-radius: 10px;
+          }
+          div:nth-child(2) {
+            flex-grow: 1;
+            height: 3px;
+            margin: 0 10px;
+          }
+          div:last-child {
+            width: 20px;
+            margin-right: -10px;
+            border-radius: 10px;
+          }
+        }
+        .bottom {
+          height: 350px;
+          /deep/ .el-scrollbar__wrap {
+            overflow-x: hidden;
+            overflow-y: scroll;
+            padding: 20px;
+            box-sizing: border-box;
+          }
+        }
+      }
+    }
+  }
 </style>
---
+++ src/pages/sale/saleInfo/index.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="shopsInfo">
+    <div class="top">
+      <div class="pic"></div>
+      <div class="information">
+        <p>TypeSomething阿特莱特男款鞋</p>
+        <p class="option">
+          <span>白色</span>
+          <span>55mm</span>
+          <span>2双</span>
+        </p>
+        <p>¥7100</p>
+      </div>
+    </div>
+    <div class="bottom">
+      <div>
+        <span>·</span>发货
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "ShopsInfo"
+  }
+</script>
+
+<style scoped lang="less">
+  .shopsInfo {
+    height: 130px;
+    color:#606266;
+    .top {
+      height: 100px;
+      display: flex;
+      .pic {
+        width: 100px;
+        height: 100%;
+        background-color: #d8d8d8;
+      }
+      .information {
+        height: 100%;
+        font-size: 13px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        width: calc(100% - 100px);
+        padding: 0 10px;
+        box-sizing: border-box;
+        .option {
+          display: flex;
+          justify-content: space-between;
+          span {
+            display: inline-block;
+            background-color: #d8d8d8;
+            padding: 0 10px;
+            border-radius: 10px;
+          }
+        }
+      }
+    }
+    .bottom {
+      height: 30px;
+      font-size: 14px;
+      display: flex;
+      align-items: center;
+      justify-content: flex-end;
+      padding: 0 10px;
+      &>div {
+        background-color: #fff;
+        border:1px solid #d8d8d8;
+        border-radius: 10px;
+        width: 60px;
+        text-align: center;
+        height: 20px;
+        &>span {
+          color: aqua;
+          font-weight: bolder;
+        }
+      }
+    }
+  }
+</style>
