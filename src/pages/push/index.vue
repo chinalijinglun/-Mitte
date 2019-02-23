12046fe0842a8a089de5c9cae0135ef16924c1ae
李经纶 <281965992@qq.com>
2019-02-24 01:16:35 +0800

    选择品类1

--- src/pages/push/index.vue
+++ src/pages/push/index.vue
@@ -1,13 +1,125 @@
 <template>
-  <div>push</div>
+  <div class="push">
+    <div class="title">
+      选择添加商品品类
+    </div>
+    <div class="push_main tableContainer">
+      <el-form ref="form" :model="form" label-width="180px" class="push_form">
+         <el-form-item label="推送文字内容">
+          <el-input type="textarea" v-model="form.detail"></el-input>
+        </el-form-item>
+        <el-form-item label="选择推送">
+        <el-select v-model="form.region" placeholder="请选择推送">
+          <el-option label="选择推送" value="1"></el-option>
+          <el-option label="选择推送" value="1"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="添加图片">
+         <el-upload
+          class="avatar-uploader"
+          action="https://jsonplaceholder.typicode.com/posts/"
+          :show-file-list="false"
+          :on-success="handleAvatarSuccess"
+          :before-upload="beforeAvatarUpload">
+          <img v-if="imageUrl" :src="imageUrl" class="avatar">
+          <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+        </el-upload>
+      </el-form-item>
+       <el-form-item>
+          <el-button type="primary" @click="onSubmit">提交</el-button>
+          <el-button>取消</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
 </template>
 
 <script>
   export default {
-    name: "index"
+    name: "index",
+    data(){
+      return{
+        form:{
+          detail:'',
+          region:''
+        },
+        imageUrl: ''
+      }
+    },
+     methods: {
+      handleAvatarSuccess(res, file) {
+        this.imageUrl = URL.createObjectURL(file.raw);
+      },
+      beforeAvatarUpload(file) {
+        const isJPG = file.type === 'image/jpeg';
+        const isLt2M = file.size / 1024 / 1024 < 2;
+
+        if (!isJPG) {
+          this.$message.error('上传头像图片只能是 JPG 格式!');
+        }
+        if (!isLt2M) {
+          this.$message.error('上传头像图片大小不能超过 2MB!');
+        }
+        return isJPG && isLt2M;
+      },
+      onSubmit(e){
+        console.log(e)
+      }
+    }
   }
 </script>
 
-<style scoped>
-
+<style scoped lang="less">
+  .push_form{
+    max-width: 750px;
+    margin-top: 100px;
+  }
+  .avatar-uploader /deep/ .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+  .avatar-uploader .el-upload:hover {
+    border-color: #409EFF;
+  }
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+  }
+.title{
+  font-size: 32px;
+  line-height: 45px;
+  margin-bottom: 25px;
+}
+.tableContainer {
+    border-radius: 7px;
+    overflow: hidden;
+    height: calc(100vh - 200px);
+    background-color: #fff;
+    /deep/ .el-table {
+      &::before {
+        background-color: transparent;
+      }
+    }
+  }
+  .orderContainer {
+    height: calc(100% - 50px);
+    /deep/ .el-row {
+      height: 100%;
+      .el-col {
+        height: 100%;
+      }
+    }
+  }
 </style>
